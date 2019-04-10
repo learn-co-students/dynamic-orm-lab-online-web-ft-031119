389527f80465f8fbeda41e2429ac36f31a92e3df
Daniel Schneider <daschne8@gmail.com>
2019-04-10 20:26:23 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,13 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+  self.column_names.each do |col_name|
+      attr_accessor col_name.to_sym
+    end
+
+  def table_name_for_insert
+    super
+    return self.class.table_name
+  end
+
 end
